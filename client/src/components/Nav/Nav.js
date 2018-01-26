51cf479b65cd63c79886dad39f4cf172fbd4db7b
dmaddox <dmaddox@gmail.com>
2018-01-25 18:29:15 -0600

    nav bar updates

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,19 +1,32 @@
 import React from "react";
+import "./Nav.css";
 
 const Nav = () =>
-  <nav className="navbar navbar-inverse navbar-top">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <button type="button" className="collapsed navbar-toggle">
-          <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar" /> <span className="icon-bar" />
-          <span className="icon-bar" />
-        </button>
-        <a href="/" className="navbar-brand">
-          New York Times Search
-        </a>
-      </div>
+  
+  <div className="container-fluid">
+  
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="col-lg-10">
+      <a className="nav-item" href="/">New York Times Search</a>
     </div>
-  </nav>;
+    <div className="col-lg-2">
+
+          <button className="navbar-toggler" type="button">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Search</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/Saved">Saved</a>
+              </li>
+            </ul>
+          </div>
+        
+    </div>
+    </nav>
+  </div>;
 
 export default Nav;
